851463ee36b4e78f02b53b7f42d9e2b47d3daf7b
Jan Kur <jankur.business@gmail.com>
2025-05-04 20:47:01 +0200

    Finish the journalCalendar component (setEntryData not implemented yet, no entry-writing area yet)

--- components/journalCalendar.jsx
+++ components/journalCalendar.jsx
@@ -3,43 +3,61 @@ import { useState } from 'react';
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function JournalCalendar() {
-    const today = new Date();
-    const [date, setDate] = useState(today);
+    const [date, setDate] = useState(new Date());
+    const [entryData, setEntryData] = useState({});
 
     function arrowLeft() {
         const newDate = new Date(date);
-        newDate.setMonth(newDate.getMonth() - 1);
+        newDate.setMonth(date.getMonth() - 1);
         setDate(newDate);
     }
-
+    
     function arrowRight() {
         const newDate = new Date(date);
-        newDate.setMonth(newDate.getMonth() + 1);
+        newDate.setMonth(date.getMonth() + 1);
         setDate(newDate);
     }
 
-    const days = Array.from({ length: 31 }, (_, i) => i + 1);
+    const todayStr = new Date().toLocaleDateString('sv-SE');
+
+    const daysInThisMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+    const firstWeekday = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+
+    const days = [];
+
+    for (let i = 1; i <= daysInThisMonth; i++) {
+    const currentDate = new Date(date.getFullYear(), date.getMonth(), i);
+    const dateStr = currentDate.toLocaleDateString('sv-SE');
+
+    days.push({
+        dayNumber: i,
+        date: dateStr,
+        isToday: dateStr === todayStr,
+        hasEntry: !!entryData[dateStr]?.hasEntry,
+    });
+    }
 
     return(
     <View className="w-fit rounded-3xl bg-darkGray p-1 border-2 border-lightGray flex-col justify-start items-center">
-        <View className="flex-row justify-center gap-5 w-fit items-center mb-6 px-2">
+        <View className="flex-row justify-center gap-5 w-fit items-center mb-3 px-2">
             <TouchableOpacity onPress={arrowLeft}>
-                <MaterialIcons name="keyboard-arrow-left" size={36} color="#6A1FCC" />
+                <MaterialIcons name="keyboard-arrow-left" size={34} color="#6A1FCC" />
             </TouchableOpacity>
             
-            <Text className="text-lightMain text-2xl font-bold">
+            <Text className="text-lightMain text-xl font-bold">
                 {date.toLocaleDateString("en-US", {month: "long", year: "numeric"})}
             </Text>
             
             <TouchableOpacity onPress={arrowRight}>
-                <MaterialIcons name="keyboard-arrow-right" size={36} color="#6A1FCC" />
+                <MaterialIcons name="keyboard-arrow-right" size={34} color="#6A1FCC" />
             </TouchableOpacity>
         </View>
 
         <View className="flex-row justify-start w-fit">
             {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(day => (
                 <View key={day} className="w-11 ml-[5px] mr-[5px] justify-center items-center flex-row" >
-                    <Text className="text-lightMain text-lg font-medium w-fit h-fit">
+                    <Text className="text-lightMain text-lg font-semibold w-fit h-fit">
                     {day}
                     </Text>
                 </View>
@@ -47,10 +65,12 @@ export default function JournalCalendar() {
         </View>
         
         <View className="flex-row flex-wrap justify-start w-[340px]">
+        {Array.from({ length: (firstWeekday + 6) % 7}).map((_, index) => (
+            <View key={`spacer-${index}`} className="w-11 h-11 m-[5px] opacity-0"/>))}
             {days.map(day => {
                 return (
-                    <TouchableOpacity key={day} className='bg-lightGray rounded-xl justify-center items-center w-11 h-11 m-[5px]'>
-                        <Text className="text-lightMain text-xl font-medium">{day}</Text>
+                    <TouchableOpacity key={day.dayNumber} className={`${day.hasEntry ? "bg-primary" : "bg-lightGray"} rounded-xl ${day.isToday ? "border-2 border-[#AC40FF]" : ""} justify-center items-center w-11 h-11 m-[5px]`}>
+                        <Text className="text-lightMain text-xl font-semibold">{day.dayNumber}</Text>
                     </TouchableOpacity>
                 );
             })}
